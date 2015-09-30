13502e9b93ca194539d36b4b5b6e458465703f30
George <gkorepanov.gk@gmail.com>
2015-09-30 20:18:11 +0300

    Errors correction

--- 2/main.c
+++ 2/main.c
@@ -4,9 +4,13 @@
 
 main() { // for check
     char string[10];
+    char* string_from_num;
     int num;
+
     scanf("%s%i", string, &num);
-    printf("%i\n%s\n", _atoi(string), _itoa(num));
+    string_from_num = _itoa(num);
+    printf("%i\n%s\n", _atoi(string), string_from_num);
+    free(string_from_num);
     return 0;
 };
 
